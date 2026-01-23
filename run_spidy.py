655df9aff63bb10f41ccbcea632e2baa3fead68f
arupanandaswain <arupaswain7735@gmail.com>
2026-01-23 11:48:35 +0530

    updated run_spidy.py for js files at the same time

--- run_spidy.py
+++ run_spidy.py
@@ -1,11 +1,17 @@
+import sys
 from scrapy.crawler import CrawlerProcess
 from utils.spider import FullWebsiteSpider
-import sys
 
 target_url = sys.argv[1]
 
 process = CrawlerProcess({
     "LOG_LEVEL": "INFO",
+    "DOWNLOAD_HANDLERS": {
+        "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+        "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    },
+    "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+    "PLAYWRIGHT_BROWSER_TYPE": "chromium",
 })
 
 process.crawl(
